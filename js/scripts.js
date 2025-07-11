c055d75af792b85c8fa8d5afc16ca56de088c746
Radyslav Dmytrenko <coder.radik@gmail.com>
2025-07-11 11:43:55 +0300

    Added styles for our team slider mobile

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,19 @@
 //Sliders
 //Our team slider
-$(document).ready(function(){
+$(document).ready(function () {
   $('.our-team__slider').slick({
     slidesToShow: 3,
     prevArrow: '<button type="button" class="slick-prev"><img src="images/icons/slider-arrow.svg" alt="slide-arrow"></button>',
     nextArrow: '<button type="button" class="slick-next"><img src="images/icons/slider-arrow.svg" alt="slide-arrow"></button>',
     appendArrows: $('.our-team__info'),
+    responsive: [
+      {
+        breakpoint: 400,
+        settings: {
+          slidesToShow: 2,
+        }
+      },
+    ]
   });
 });
 
