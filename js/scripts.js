833ff26e9a78ea2cb5874ddc125426c125432e24
Radyslav Dmytrenko <coder.radik@gmail.com>
2025-07-19 16:07:13 +0300

    Added SEO tegs and contact form back-end

--- js/scripts.js
+++ js/scripts.js
@@ -17,44 +17,6 @@ $(document).ready(function () {
   });
 });
 
-//Contact us form
-document.addEventListener("DOMContentLoaded", function () {
-  const body = document.querySelector('body');
-  const form = document.getElementById('form');
-  form.addEventListener("submit", formSend);
-
-  //Send form
-  async function formSend(e) {
-    e.preventDefault();
-
-    let error = formValidate(form);
-
-    let formData = new FormData(form);
-
-    if (error === 0) {
-      form.classList.add('_sending');
-      body.classList.add('_sending');
-      let response = await fetch('sendmail.php', {
-        method: 'POST',
-        body: formData,
-      });
-
-      if (response.ok) {
-        let result = await response.json();
-        alert(result.message);
-        form.reset();
-        form.classList.remove('_sending');
-        body.classList.remove('_sending');
-      } else {
-        alert('Виявлено неполадки зі сторони сервера. Тривають відновлювальні роботи. Повторіть спробу пізніше');
-      }
-    } else {
-      alert("Заповніть обов'язкові поля!");
-    }
-  }
-
-});
-
 //Variables
 const burgerBtn = document.querySelector('.burger-menu');
 const menu = document.querySelector('.menu');
@@ -77,32 +39,48 @@ function closeMenuOnClick() {
   })
 }
 
-//Form validate
-function formValidate(form) {
-  let error = 0;
-  let formReq = document.querySelectorAll('._req');
+//Send form
+const ERROR_MESSAGE = 'ПОМИЛКА З БОКУ СЕРВЕРА!';
+const SUCCESS_MESSAGE = 'ЗАЯВКУ ВІДПРАВЛЕНО!';
+let colorMessage;
 
-  for (let index = 0; index < formReq.length; index++) {
-    const input = formReq[index];
-    formRemoveError(input);
+const form = document.querySelector('.form-data');
+form.addEventListener("submit", formSend);
 
-    if(input.value === '') {
-      formAddError(input);
-      error++;
-    }
+async function formSend(event) {
+  event.preventDefault();
+
+  const formData = new FormData(form);
+
+  let response = await fetch('php/sendmail.php', {
+    method: 'POST',
+    body: formData,
+    mode: "no-cors",
+  });
+
+  if (response.ok) {
+    colorMessage = 'linear-gradient(to right, #00b09b, #96c93d)';
+    showMessage(SUCCESS_MESSAGE, colorMessage);
+    form.reset();
+  } else {
+    colorMessage = 'linear-gradient(to right, red, red)';
+    showMessage(ERROR_MESSAGE, colorMessage);
   }
-  return error;
 }
 
-//Functions helpers for form
-function formAddError(input) {
-  input.parentElement.classList.add('_error');
-  input.classList.add('_error');
-}
-
-function formRemoveError(input) {
-  input.parentElement.classList.remove('_error');
-  input.classList.remove('_error');
+function showMessage(message, colorMessage) {
+  Toastify({
+    text: message,
+    duration: 5000,
+    newWindow: true,
+    close: true,
+    gravity: "top", // `top` or `bottom`
+    position: "center", // `left`, `center` or `right`
+    stopOnFocus: true, // Prevents dismissing of toast on hover
+    style: {
+      background: colorMessage,
+    },
+  }).showToast();
 }
 
 //Call functions
---
+++ php/sendmail.php
@@ -0,0 +1,41 @@
+<?php
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
+
+//Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
+$mail->CharSet = 'UTF-8';
+$mail->setLanguage('ua', 'phpmailer/language/');
+
+try {
+    //Server settings
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'biurokoshtorysiv@gmail.com';                     //SMTP username
+    $mail->Password   = 'hwhvlhzbhbbpyvdv';                               //SMTP password
+    $mail->SMTPSecure = 'TLS';            //Enable implicit TLS encryption
+    $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    //Recipients
+    $mail->setFrom('biurokoshtorysiv@gmail.com', 'Клієнт');
+    $mail->addAddress('kalmukova27@ukr.net', 'Валентина Савчук');     //Add a recipient
+    $body = '<p>Клієнт: '.$_POST['name'].'</p>' . '<p>Номер телефона: '.$_POST['phone'].'</p>';
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'Лист від клієнта Бюро Кошторисів';
+    $mail->Body    = $body;
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
